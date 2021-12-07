7ccc83f88eab60b3404d1de4b978125a5e2a23aa
luiz-gustavo0 <santosluizgustavo101@gmail.com>
2021-12-06 23:49:12 -0300

    add responsive layout

--- src/Loading/index.jsx
+++ src/Loading/index.jsx
@@ -4,14 +4,8 @@ import './styles.scss';
 
 export const Loading = () => {
   return (
-    // <div class='loadingio-spinner-eclipse-3nfxmyrhmzw'>
-    //   <div class='ldio-4k5gj1h3z9x'>
-    //     <div></div>
-    //   </div>
-    // </div>
-
-    <div class='wrapper'>
-      <div class='spiner'>
+    <div className='wrapper'>
+      <div className='spiner'>
         <div></div>
       </div>
     </div>
--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -11,6 +11,13 @@ export const Footer = () => {
       <div className='footer__container'>
         <p className='footer__copy'>2021 &copy; - Luiz Gustavo Santos</p>
 
+        <p className='footer__atribuition'>
+          Fotos fornecidas por{' '}
+          <a href='https://pexels.com' target='_blank' rel='noreferrer'>
+            pexels.com
+          </a>
+        </p>
+
         <div className='social-links'>
           <a
             href='https://linkedin.com/in/luiz-gustavo0'
@@ -30,12 +37,6 @@ export const Footer = () => {
           </a>
         </div>
       </div>
-      {/* <p className='footer__atribuition'>
-        Fotos fornecidas por{' '}
-        <a href='https://pexels.com' target='_blank' rel='noreferrer'>
-          pexels.com
-        </a>
-      </p> */}
     </footer>
   );
 };
